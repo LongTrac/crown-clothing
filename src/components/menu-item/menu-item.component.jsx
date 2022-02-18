fc749bb14cccadfab02775225da03fcafce33a74
Long Trac <lokesp123@gmail.com>
2022-02-17 21:01:36 -0600

    Forget to save 1 file

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,7 +3,7 @@ import './menu-item.style.scss'
 
 const MenuItem = ({title, imgUrl, size}) => (
     <div className={`${size} menu-item`}>
-        <div className="background-image "
+        <div className="background-image"
         //style takes in an obj that has value === to the css value you want to apply to this elem 
         // the first pair of parenthesis is the pair to signify that we are going to have code in here. The second pair is an object bc style takes in an object
         // ex: let person = {fname:'J', lname:'D',phone:'xxx'}
